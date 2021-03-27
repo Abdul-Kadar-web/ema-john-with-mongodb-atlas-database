f190f012294127cf73fafd120262d3ea58a9f714
Abdul-Kadar-web <emonkhan016844@gmail.com>
2021-03-27 14:50:03 +0600

    ema john with mongodb database

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCaM4CfvFAh9bLfvysDh-6zfic0SJF3K4w",
+  authDomain: "ema-john-with-mongodb.firebaseapp.com",
+  projectId: "ema-john-with-mongodb",
+  storageBucket: "ema-john-with-mongodb.appspot.com",
+  messagingSenderId: "785009432261",
+  appId: "1:785009432261:web:0a3363cff990789caa02ac"
+};
+  export default firebaseConfig;
